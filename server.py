eee485e6127f126f8c3346d89e21a7037d6f0c08
EC2 Default User <ec2-user@ip-172-31-15-27.us-east-2.compute.internal>
2024-01-24 01:51:29 +0000

    corrected server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask import jsonify
-from flask import render_template, request, redirect
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def index():
   return render_template('index.html')
-
+  
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
